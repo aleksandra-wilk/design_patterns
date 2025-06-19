343f9610b12c3876a11b6e4f29e49087a6a97511
Aleksandra Wilk <wilkaleksandra@outlook.com>
2025-06-19 13:28:41 +0200

    pep8

--- strategy.py
+++ strategy.py
@@ -1,11 +1,13 @@
 from abc import ABC, abstractmethod
 
+
 # Strategy
 class Strategy(ABC):
-    
+
     @abstractmethod
     def execute(self):
         pass
+
 
 # Concreate Strategy 1
 class Flying(Strategy):
@@ -13,18 +15,21 @@ class Flying(Strategy):
     def execute(self):
         print("I'm flying")
 
+
 # Concrete Strategy 2
 class Swimming(Strategy):
 
     def execute(self):
         print("I'm swimming")
 
+
 # Concreate Strategy 3
 class Walking(Strategy):
 
     def execute(self):
         print("I'm walking")
-    
+
+
 # Context
 class Duck:
 
@@ -45,5 +50,6 @@ def main():
     d1.move()
     d1.set_moving_type(Walking())
     d1.move()
+
 
 main()
